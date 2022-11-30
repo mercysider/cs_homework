68a8ed434d4b8f78607266da821ab01657553938
mercysider <mercysideru@maile.ru>
2022-11-30 18:03:22 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+string userNum = Console.ReadLine();
+
+int IntNum = Convert.ToInt32(userNum); 
+
+if (IntNum %2 == 0) Console.Write("Введено четное число");
+else Console.Write("Введено нечетное число");
