cc4e1bff4a888a6d423ddb71a54c3114c4a6e8fb
mercysider <mercysideru@maile.ru>
2022-11-30 18:27:57 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+string userNum = Console.ReadLine();
+int IntNum = Convert.ToInt32(userNum); 
+int i = 1; //счетчик
+
+if (IntNum <= 1) 
+{
+Console.Write("Нет четных чисел от 1 до ");
+Console.Write(IntNum);
+}
+else 
+{
+if (IntNum %2 != 0) IntNum = IntNum - 1; 
+
+    while (i < IntNum/2)
+    {
+        Console.Write(i*2 + ", ");
+        i++;
+    }
+    Console.Write(IntNum);
+}
