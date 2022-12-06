0de87b5dfc1e27e2155721e47c004dc04d321a63
mercysider <mercysideru@maile.ru>
2022-12-06 16:48:29 +0300

    task 10 is ready

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100,1000);
+int secondDigit;
+string answer;
+
+secondDigit = (number / 10) % 10;
+answer = "Вторая цифра в числе " + number + ": " + secondDigit;
+
+Console.WriteLine(answer);
