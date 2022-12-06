045050fe44067f40c71f6a4f7167619afebe774c
mercysider <mercysideru@maile.ru>
2022-12-06 17:28:30 +0300

    task 10 add second solution

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,8 +1,20 @@
-﻿int number = new Random().Next(100,1000);
-int secondDigit;
-string answer;
+﻿// решение для случайного положительного трехзначного числа
+//int number = new Random().Next(100,1000); 
+//int secondDigit;
+//string answer;
+//secondDigit = (number / 10) % 10;
+//answer = "Вторая цифра в числе " + number + ": " + secondDigit;
+//Console.WriteLine(answer);
 
-secondDigit = (number / 10) % 10;
-answer = "Вторая цифра в числе " + number + ": " + secondDigit;
+// решение для числа от пользователя
+Console.WriteLine("Введите трехзначное число:");
+string userNum = Console.ReadLine()!;
+int intNum;
 
-Console.WriteLine(answer);
+if (int.TryParse(userNum, out intNum)) // проверяем, является ли введенная строка int-ом
+{
+    userNum = userNum.TrimStart('-','0'); // убираем в начале введенной строки минус и нули
+    if (userNum.Length != 3) Console.WriteLine("Число " + intNum + " не явялется трехзначным!"); // проверка на трехзначность
+    else Console.WriteLine("Вторая цифра числа " + intNum + ": " + userNum[1]); // вывод второго разряда введенного числа
+}
+else Console.WriteLine("Введено не число типа int!"); // результат проверки - ввели не число
